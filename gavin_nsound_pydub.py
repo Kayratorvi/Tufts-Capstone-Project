73ed66576071039dcd7d2f5c8cc9d9184a6742f6
Kayratorvi <gavin.grooms@outlook.com>
2023-11-10 10:45:52 -0700

    Adjusted functions to handle chord creation

--- gavin_nsound_pydub.py
+++ gavin_nsound_pydub.py
@@ -1,10 +1,12 @@
 from Nsound import *
 from pydub import AudioSegment
+import os
+import sys
 
 sr = 44100.0
 BITS_PER_SAMPLE = 24
 
-def chord(sr, duration, f1, f2, gaussian_width):
+def stereo_note(sr, duration, f1, f2, gaussian_width):
 
     sin = Sine(sr)
 
@@ -17,7 +19,7 @@ def chord(sr, duration, f1, f2, gaussian_width):
 
     return audio * envelope
 
-def note(sr, duration, f1, gaussian_width):
+def mono_note(sr, duration, f1, gaussian_width):
 
     sin = Sine(sr)
 
@@ -29,30 +31,38 @@ def note(sr, duration, f1, gaussian_width):
 
     return audio * envelope
 
-sine = Sine(sr)
+def chord(sr, base_frequency, duration, gaussian_width, name):
 
-c = 258
+    name1 = "1" + name
+    name2 = "2" + name
 
-out1 = AudioStream(sr, 2)
+    sine = Sine(sr)
 
-out1 << chord(sr, 0.75, c, (c * 5 / 4), 0.90) \
-    << sine.silence(0.5)
+    out1 = AudioStream(sr, 2)
 
-out1 >> "gavin_nsound_ji1.wav"
+    out1 << stereo_note(sr, duration, base_frequency, (base_frequency * 5 / 4), gaussian_width) \
+        << sine.silence(0.5)
 
-out2 = AudioStream(sr, 1)
+    out1 >> name1
 
-out2 << note(sr, 0.75, (c * 3 / 2), 0.90)
-out2 << sine.silence(0.5)
+    out2 = AudioStream(sr, 1)
 
-out2 >> "gavin_nsound_ji2.wav"
+    out2 << mono_note(sr, duration, (base_frequency * 3 / 2), gaussian_width)
+    out2 << sine.silence(0.5)
 
-sound1 = AudioSegment.from_file("./gavin_nsound_ji1.wav", format="wav")
-sound2 = AudioSegment.from_file("./gavin_nsound_ji2.wav", format="wav")
+    out2 >> name2
 
-sound1 = sound1 - 7
-sound2 = sound2 - 7
+    sound1 = AudioSegment.from_file(name1, format="wav")
+    sound2 = AudioSegment.from_file(name2, format="wav")
 
-combined = sound1.overlay(sound2)
+    sound1 = sound1 - 7
+    sound2 = sound2 - 7
 
-file_handle = combined.export("./gavin_nsound_ji3.wav", format="wav")
+    combined = sound1.overlay(sound2)
+
+    file_handle = combined.export(name, format="wav")
+    os.remove(name1)
+    os.remove(name2)
+
+chord(sr, 260, 1.0, .90, "gavin_test_chord.wav")
+sys.exit()
