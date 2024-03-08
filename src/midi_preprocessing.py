0d7012d8a22771a535d2511620e2bf85c20263a7
Kayratorvi <gavin.grooms@outlook.com>
2024-03-07 18:53:52 -0700

    Added new logic to separate concurrent notes from nearby notes Also added index to each MidiMessageExt for easy reference.

--- src/midi_preprocessing.py
+++ src/midi_preprocessing.py
@@ -5,8 +5,10 @@ log = open("log.txt", "w")
 def process_note_lengths(midi):
     queue = list()
     totalDeltaTime = 0
+    index = -1
 
     for message in midi:
+        index = index + 1
         foundNote = False
         totalDeltaTime += message.time
         if not message.is_meta:
@@ -30,30 +32,33 @@ def process_note_lengths(midi):
 
 def link_notes(notes, ticks_per_beat):
     # This loop adds notes that end within one beat or after the current note
-    for index, note in enumerate(notes):
-        for to_link_index in range(index - 1, -1, -1):
+    for i, note in enumerate(notes):
+        note.set_index(i)
+        for to_link_index in range(i - 1, -1, -1):
             if notes[to_link_index].note_ends_timestamp() > (note.timestamp - ticks_per_beat):
                 # note is being played within one beat earlier of current note
-                note.link_note(notes[to_link_index], to_link_index)
+                note.link_note(notes[to_link_index])
             else:
                 break # there may be notes that start much earlier but have a long duration, will consider in next loop section
 
+        # This loop handles any note that starts much earlier in 
+        # the music and has an exceptionally long duration that 
+        # extends much longer than notes that start after this note 
+        # but before the note in the linking step. These notes will 
+        # already be added in previous notes and will be added by
+        # looking at the linked notes of all currently linked notes
         for linked_note in note.linked_notes:
             for linked_note_linked_note in linked_note.linked_notes:
                 if (linked_note_linked_note.note_ends_timestamp() > (note.timestamp - ticks_per_beat)) and linked_note_linked_note not in note.linked_notes:
-                    note.link_note(linked_note_linked_note, notes.index(linked_note_linked_note))
-                    # This loop handles any note that starts much earlier in 
-                    # the music and has an exceptionally long duration that 
-                    # extends much longer than notes that start after this note 
-                    # but before the note in the linking step. These notes will 
-                    # already be added in previous notes and will be added by
-                    # looking at the linked notes of all currently linked notes
-        
+                    note.link_note(linked_note_linked_note)
+                            
     # This loop adds all notes that start within one beat after the current note
-    for index, note in enumerate(notes):
-        for to_link_index2 in range(index + 1, len(notes), 1):
+    for i, note in enumerate(notes):
+        for to_link_index2 in range(i + 1, len(notes), 1):
             if notes[to_link_index2].timestamp < (note.timestamp + ticks_per_beat):
                 # note begins within one beat later of current note
-                note.link_note(notes[to_link_index2], to_link_index2)
+                note.link_note(notes[to_link_index2])
             else:
                 break # all other notes begin later in music
+        
+        note.separate_notes_by_concurrency()
--- src/midimessageext.py
+++ src/midimessageext.py
@@ -4,17 +4,18 @@ class MidiMessageExt:
         self.note = note
         self.velocity = velocity
         self.timestamp = timestamp
+        self.i = -1
         self.note_length = -1 
         self.frequency = -1.0 
         self.duration_seconds = -1.0 
         self.linked_notes = list()
-        self.linked_indices = list()
+        self.nearby_notes = list()
 
     def __repr__(self):
-        return f"\nMidiMessageExt(channel={self.channel}, note={self.note}, velocity={self.velocity}, timestamp={self.timestamp}, note_length={self.note_length}, tuning={self.frequency}, duration_seconds={self.duration_seconds})"
+        return f"\nMidiMessageExt(channel={self.channel}, note={self.note}, velocity={self.velocity}, timestamp={self.timestamp}, note_length={self.note_length}, tuning={self.frequency}, duration_seconds={self.duration_seconds}, index={self.i})"
 
     def __str__(self):
-        return f"\nMidiMessageExt(channel={self.channel}, note={self.note}, velocity={self.velocity}, timestamp={self.timestamp}, note_length={self.note_length}, tuning={self.frequency}, duration_in_seconds={self.duration_seconds})"
+        return f"\nMidiMessageExt(channel={self.channel}, note={self.note}, velocity={self.velocity}, timestamp={self.timestamp}, note_length={self.note_length}, tuning={self.frequency}, duration_in_seconds={self.duration_seconds}, index={self.i})"
 
     def change_frequency(self, frequency):
         self.frequency = frequency
@@ -26,9 +27,55 @@ class MidiMessageExt:
         self.duration_seconds = (float(self.note_length) / ticks_per_beat) * (tempo / 1000000.0)
         return self.duration_seconds
     
-    def link_note(self, note, index):
+    def link_note(self, note):
         self.linked_notes.append(note)
-        self.linked_indices.append(index)
+    
+    # Separating nearby notes from concurrent notes
+    def relate_note(self, note):
+        self.linked_notes.remove(note)
+        self.nearby_notes.append(note)
 
     def note_ends_timestamp(self):
         return self.timestamp + self.note_length
+    
+    def separate_notes_by_concurrency(self):
+        notes_to_move = list()
+        for note in self.linked_notes:
+            if note.note_ends_timestamp() < self.timestamp:
+                notes_to_move.append(note)
+            elif self.timestamp > note.note_ends_timestamp():
+                notes_to_move.append(note)
+
+        for note in notes_to_move:
+            self.relate_note(note)
+    
+    def set_index(self, index):
+        self.i = index
+
+    def unique_linked_notes(self):
+        note_nums = list()
+        unique_nums = list()
+
+        for note in self.linked_notes:
+            note_nums.append(note.note)
+
+        for num in note_nums:
+            num = num % 12
+            if num not in unique_nums:
+                unique_nums.append(num)
+        
+        return unique_nums
+    
+    def unique_nearby_notes(self):
+        note_nums = list()
+        unique_nums = list()
+
+        for note in self.nearby_notes:
+            note_nums.append(note.note)
+
+        for num in note_nums:
+            num = num % 12
+            if num not in unique_nums:
+                unique_nums.append(num)
+        
+        return unique_nums
